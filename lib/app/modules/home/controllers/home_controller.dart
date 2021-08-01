ce5d78c62cd46e00bd26090cfa4a266c3c79341d
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-08-01 17:36:11 +0700

    commit for course 35(yt)

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  Stream<DocumentSnapshot<Map<String, dynamic>>> chatsStream(String email) {
+    return firestore.collection('users').doc(email).snapshots();
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,41 +1,14 @@
+import 'package:chatapp/app/controllers/auth_controller.dart';
 import 'package:chatapp/app/routes/app_pages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  final List<Widget> myChats = List.generate(
-    20,
-    (index) => ListTile(
-      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.black26,
-        child: Image.asset(
-          "assets/logo/noimage.png",
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        "Orang ke ${index + 1}",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      subtitle: Text(
-        "status orang ke ${index + 1}",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-      trailing: Chip(
-        label: Text("3"),
-      ),
-    ),
-  ).reversed.toList();
+  final authC = Get.find<AuthController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,10 +57,49 @@ class HomeView extends GetView<HomeController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.zero,
-              itemCount: myChats.length,
-              itemBuilder: (context, index) => myChats[index],
+            child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+              stream: controller.chatsStream(authC.user.value.email!),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  var allChats = (snapshot.data!.data()
+                      as Map<String, dynamic>)["chats"] as List;
+                  return ListView.builder(
+                    padding: EdgeInsets.zero,
+                    itemCount: allChats.length,
+                    itemBuilder: (context, index) => ListTile(
+                      onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.black26,
+                        child: Image.asset(
+                          "assets/logo/noimage.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      title: Text(
+                        "Orang ke ${index + 1}",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      subtitle: Text(
+                        "status orang ke ${index + 1}",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      trailing: Chip(
+                        label: Text("3"),
+                      ),
+                    ),
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
           ),
         ],
