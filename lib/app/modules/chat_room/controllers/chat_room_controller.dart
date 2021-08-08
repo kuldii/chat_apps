54e1f0a07294066db1e913372ea8358c08f19b86
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-08-08 13:59:34 +0700

    commit for course 43(yt)

--- lib/app/modules/chat_room/controllers/chat_room_controller.dart
+++ lib/app/modules/chat_room/controllers/chat_room_controller.dart
@@ -12,6 +12,12 @@ class ChatRoomController extends GetxController {
   late FocusNode focusNode;
   late TextEditingController chatC;
 
+  Stream<QuerySnapshot<Map<String, dynamic>>> streamChats(String chat_id) {
+    CollectionReference chats = firestore.collection("chats");
+
+    return chats.doc(chat_id).collection("chat").orderBy("time").snapshots();
+  }
+
   void addEmojiToChat(Emoji emoji) {
     chatC.text = chatC.text + emoji.emoji;
   }
@@ -75,6 +81,8 @@ class ChatRoomController extends GetxController {
         "total_unread": 1,
       });
     }
+
+    chatC.clear();
   }
 
   @override
--- lib/app/modules/chat_room/views/chat_room_view.dart
+++ lib/app/modules/chat_room/views/chat_room_view.dart
@@ -1,13 +1,14 @@
 import 'package:chatapp/app/controllers/auth_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/chat_room_controller.dart';
 
 class ChatRoomView extends GetView<ChatRoomController> {
   final authC = Get.find<AuthController>();
+  final String chat_id = (Get.arguments as Map<String, dynamic>)["chat_id"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,15 +65,24 @@ class ChatRoomView extends GetView<ChatRoomController> {
           children: [
             Expanded(
               child: Container(
-                child: ListView(
-                  children: [
-                    ItemChat(
-                      isSender: true,
-                    ),
-                    ItemChat(
-                      isSender: false,
-                    ),
-                  ],
+                child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                  stream: controller.streamChats(chat_id),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.active) {
+                      var alldata = snapshot.data!.docs;
+                      return ListView.builder(
+                        itemCount: alldata.length,
+                        itemBuilder: (context, index) => ItemChat(
+                          msg: "${alldata[index]["msg"]}",
+                          isSender: alldata[index]["pengirim"] ==
+                                  authC.user.value.email!
+                              ? true
+                              : false,
+                        ),
+                      );
+                    }
+                    return Center(child: CircularProgressIndicator());
+                  },
                 ),
               ),
             ),
@@ -176,9 +186,11 @@ class ItemChat extends StatelessWidget {
   const ItemChat({
     Key? key,
     required this.isSender,
+    required this.msg,
   }) : super(key: key);
 
   final bool isSender;
+  final String msg;
 
   @override
   Widget build(BuildContext context) {
@@ -208,7 +220,7 @@ class ItemChat extends StatelessWidget {
             ),
             padding: EdgeInsets.all(15),
             child: Text(
-              "Haloo selamat datang",
+              "$msg",
               style: TextStyle(
                 color: Colors.white,
               ),
